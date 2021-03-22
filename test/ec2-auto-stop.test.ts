06f39dc14dcd6cf510b2e3ec489889fd735f597b
Lucky <lucky3028@users.noreply.github.com>
2021-03-22 23:11:50 +0900

    [Remove] 不要なコード

--- test/ec2-auto-stop.test.ts
+++ test/ec2-auto-stop.test.ts
@@ -1,13 +1,9 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { matchTemplate, MatchStyle, SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Ec2AutoStop from '../lib/ec2-auto-stop-stack';
 
-test('Empty Stack', () => {
+test('Generate stack', () => {
     const app = new cdk.App();
-    // WHEN
     const stack = new Ec2AutoStop.Ec2AutoStopStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
 });
