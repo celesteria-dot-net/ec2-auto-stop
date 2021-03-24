5cdb09c47c2392b920d39f794f4349520c5cfb25
Lucky <lucky3028@users.noreply.github.com>
2021-03-24 22:23:33 +0900

    [Format] Lint

--- bin/ec2-auto-stop.ts
+++ bin/ec2-auto-stop.ts
@@ -4,4 +4,5 @@ import * as cdk from '@aws-cdk/core';
 import { Ec2AutoStopStack } from '../lib/ec2-auto-stop-stack';
 
 const app = new cdk.App();
+// eslint-disable-next-line no-new
 new Ec2AutoStopStack(app, 'Ec2AutoStopStack');
--- lib/ec2-auto-stop-stack.ts
+++ lib/ec2-auto-stop-stack.ts
@@ -1,43 +1,50 @@
-import * as cdk from "@aws-cdk/core";
-import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
-import { PolicyStatement } from "@aws-cdk/aws-iam";
+import * as cdk from '@aws-cdk/core';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
 import { Rule, Schedule } from '@aws-cdk/aws-events';
-import { LambdaFunction } from "@aws-cdk/aws-events-targets";
+import { LambdaFunction } from '@aws-cdk/aws-events-targets';
 
+// eslint-disable-next-line import/prefer-default-export
 export class Ec2AutoStopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const policyStatements = [
       new PolicyStatement({
-        resources: [`arn:aws:ec2:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:instance/*`],
-        actions: ["ec2:StopInstances"],
+        resources: [
+          `arn:aws:ec2:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:instance/*`,
+        ],
+        actions: ['ec2:StopInstances'],
       }),
       // DescribeInstancesはリソースの指定ができない(ref. https://dev.classmethod.jp/articles/tsnote-iam-resource-level-001/)
       new PolicyStatement({
         resources: ['*'],
-        actions: ["ec2:DescribeInstances"],
-      })
-    ]
+        actions: ['ec2:DescribeInstances'],
+      }),
+    ];
 
-    const stopFunc = new NodejsFunction(this, "stop-func");
+    const stopFunc = new NodejsFunction(this, 'stop-func');
     policyStatements.forEach((policy) => {
       stopFunc.addToRolePolicy(policy);
     });
 
     const stopFuncTriggers = [
-      new Rule(this, "stop-func-schedule_1430", {
+      new Rule(this, 'stop-func-schedule_1430', {
         schedule: Schedule.cron({
-          minute: "30", hour: "14"
-        })
+          minute: '30',
+          hour: '14',
+        }),
       }),
-      new Rule(this, "stop-func-schedule_1440", {
+      new Rule(this, 'stop-func-schedule_1440', {
         schedule: Schedule.cron({
-          minute: "40", hour: "14"
-        })
-      })
-    ]
+          minute: '40',
+          hour: '14',
+        }),
+      }),
+    ];
 
-    stopFuncTriggers.forEach((rule) => rule.addTarget(new LambdaFunction(stopFunc)))
+    stopFuncTriggers.forEach((rule) =>
+      rule.addTarget(new LambdaFunction(stopFunc)),
+    );
   }
 }
