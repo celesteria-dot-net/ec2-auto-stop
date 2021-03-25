2fef3f291040a56125e4c7968d6aa3e54a1d3ace
Lucky <lucky3028@users.noreply.github.com>
2021-03-25 20:09:58 +0900

    [Add] test

--- test/ec2-auto-stop.test.ts
+++ test/ec2-auto-stop.test.ts
@@ -1,9 +1,64 @@
+import '@aws-cdk/assert/jest';
 import { SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Ec2AutoStop from '../lib/ec2-auto-stop-stack';
 
-test('Ec2AutoStopStack Snapshot Test', () => {
+describe('Ec2AutoStopStack Snapshot Test', () => {
   const app = new cdk.App();
-  const stack = new Ec2AutoStop.Ec2AutoStopStack(app, 'MyTestStack');
-  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  const stack = new Ec2AutoStop.Ec2AutoStopStack(app, 'Ec2AutoStopStack');
+  test('stack', () => {
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
+});
+
+describe('Ec2AutoStopStack Fine-Grained Test', () => {
+  const app = new cdk.App();
+  const stack = new Ec2AutoStop.Ec2AutoStopStack(app, 'Ec2AutoStopStack');
+
+  test('Lambda', () => {
+    expect(stack).toHaveResource('AWS::Lambda::Function', {
+      Handler: 'index.handler',
+    });
+  });
+
+  test('IAM Policy', () => {
+    expect(stack).toHaveResource('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 'ec2:StopInstances',
+            Effect: 'Allow',
+            Resource: {
+              'Fn::Join': [
+                '',
+                [
+                  'arn:aws:ec2:',
+                  {
+                    Ref: 'AWS::Region',
+                  },
+                  ':',
+                  {
+                    Ref: 'AWS::AccountId',
+                  },
+                  ':instance/*',
+                ],
+              ],
+            },
+          },
+          {
+            Action: 'ec2:DescribeInstances',
+            Effect: 'Allow',
+            Resource: '*',
+          },
+        ],
+        Version: '2012-10-17',
+      },
+    });
+  });
+
+  test('Events Rule', () => {
+    expect(stack).toHaveResource('AWS::Events::Rule', {
+      ScheduleExpression: 'cron(30,40 14 * * ? *)',
+    });
+  });
 });
