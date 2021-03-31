db5264a8b1647b1c973594380ccdaca60ed18818
Lucky <lucky3028@users.noreply.github.com>
2021-03-31 16:33:29 +0900

    [Add] インスタンスを起動する関数

---
+++ lib/ec2-auto-stop-stack.start-func.ts
@@ -0,0 +1,38 @@
+import { Filter } from '@aws-sdk/client-ec2';
+import { startedInstances, noInstancesToRun } from './domains/webhookContents';
+import postToDiscord from './util/discord';
+import { fetchInstanceIds, startInstances } from './util/ec2';
+
+const tagNames = ['bot_container']
+
+const filters: Filter[] = [
+  { Name: 'tag-key', Values: tagNames },
+]
+
+// eslint-disable-next-line import/prefer-default-export
+export const handler = async (): Promise<{
+  isSuccessful: boolean;
+  error?: unknown;
+}> => {
+  try {
+    const instanceIds = await fetchInstanceIds(filters);
+    if (instanceIds.length !== 0) await startInstances(instanceIds);
+
+    const webhookMessage = instanceIds.length === 0 ? noInstancesToRun : startedInstances
+    const response = await postToDiscord(instanceIds, webhookMessage);
+    if (response.status !== 204)
+      return {
+        isSuccessful: false,
+        error: `Webhook error: ${response.statusText}`,
+      };
+
+    return {
+      isSuccessful: true,
+    };
+  } catch (err) {
+    return {
+      isSuccessful: false,
+      error: err,
+    };
+  }
+};
